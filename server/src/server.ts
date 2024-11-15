7e830f60afd3f3cf2aac98e57791b5aaa116dd0f
sashamolotovs7 <alekcvolkov7@gmail.com>
2024-11-14 21:30:00 -0500

    Save changes before switching to develop

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import path from 'node:path';
 import type { Request, Response } from 'express';
 import db from './config/connection.js'
-import { ApolloServer } from '@apollo/server';// Note: Import from @apollo/server-express
+import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@apollo/server/express4';
 import { typeDefs, resolvers } from './schemas/index.js';
 import { authenticateToken } from './services/auth.js';
@@ -57,8 +57,8 @@ const startApolloServer = async () => {
 
   // Start the server
   app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
-    console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+    console.log(`🌍 Movie app is running on port ${PORT}!`);
+    console.log(`🚀 GraphQL endpoint available at http://localhost:${PORT}/graphql`);
   });
 };
 
