a11f3a81cbf06c7bff9967e2e004b479109dd912
reisleo99 <leonardo.ao.reis@gmail.com>
2024-11-16 19:30:20 -0500

    Changed movie interface to match response from API. Updated searchmovies page to handle add to Seen It and Next Up.

--- client/src/models/Movie.ts
+++ client/src/models/Movie.ts
@@ -1,16 +1,16 @@
 export interface Movie {
   adult: boolean;
-  backdrop_path: string | null;
-  genre_ids: number[];
+  backdropPath: string | null;
+  genreIds: number[];
   id: number;
-  original_language: string;
-  original_title: string;
+  originalLanguage: string;
+  originalTitle: string;
   overview: string;
   popularity: number;
-  poster_path: string | null;
-  release_date: string;
+  posterPath: string | null;
+  releaseDate: string;
   title: string;
   video: boolean;
-  vote_average: number;
-  vote_count: number;
+  voteAverage: number;
+  voteCount: number;
 }
