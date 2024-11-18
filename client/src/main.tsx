0f6f75514369aad4afdd2813daf65435e7a213c9
@selmgar-eEHSZr <selmgar@gmail.com>
2024-11-18 16:35:52 -0600

    Removes .js from the Trending page

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,10 @@
-import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-import App from './App.jsx'
-import Landing from './pages/Landing'
-import Trending from './pages/Trending.js'
+import App from './App.jsx';
+import Landing from './pages/Landing';
+import Trending from './pages/Trending';
 // import SignupForm from './components/SignupForm'
 // import LoginForm from './components/LoginForm'
 
