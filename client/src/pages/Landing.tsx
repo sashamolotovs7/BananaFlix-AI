a2180b1b39cc44d34f2d082a7ca2a11661bbc365
bmw-dev0p <bmw.dev0p@gmail.com>
2024-11-17 20:09:31 -0500

    adding img for poster

--- client/src/pages/Landing.tsx
+++ client/src/pages/Landing.tsx
@@ -1,11 +1,12 @@
 // import { Link } from "react-router-dom"
-import React from "react"
-
+// import React from "react"
+import poster from '../assets/bananaFlix.jpg'
 
 const Landing = () => {
     return (
     <div>
         <h1>Landing Page</h1>
+        <img className='pilot-logo' src={poster} alt='pilot-logo' />
     </div>
 )}
 
