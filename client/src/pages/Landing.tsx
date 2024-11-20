b09b1abe8bc5094cf447756b27b011399435830a
bmw-dev0p <bmw.dev0p@gmail.com>
2024-11-19 21:15:53 -0500

    fixed login / signup CSS buttons

--- client/src/pages/Landing.tsx
+++ client/src/pages/Landing.tsx
@@ -1,22 +1,45 @@
-import { Link } from "react-router-dom";
-import poster from '../assets/bananaFlix.jpg';
+import React from 'react';
+import poster from "../assets/bananaFlix.jpg";
 import './Landing.css';
 
-const Landing = () => {
+// interface LandingProps {
+//     setShowModal: React.Dispatch<React.SetStateAction<boolean>>;
+//     setActiveTab: React.Dispatch<React.SetStateAction<string>>;
+// }
+
+const Landing = ({
+    setShowModal,
+    setActiveTab
+}: {
+    setShowModal: React.Dispatch<React.SetStateAction<boolean>>;
+    setActiveTab: React.Dispatch<React.SetStateAction<string>>;
+}) => {
     return (
-        <div className='container1'>
-            <img className='poster' src={poster} alt='banana-poster' />
-            <div className='content'>
+        <div className="container1">
+            <img className="poster" src={poster} alt="banana-poster" />
+            <div className="content">
                 <h1>BananaFlix</h1>
                 <h2>Watch a movie, eat a banana</h2>
                 <p>Sign up to search for your favorite movies and TV shows!</p>
-                <div className='button-container'>
-                    <Link to="/login">
-                        <button className='land-btn'>Login</button>
-                    </Link>
-                    <Link to="/signup">
-                        <button className='land-btn'>Sign Up</button>
-                    </Link>
+                <div className="button-container">
+                    <button
+                        className="land-btn"
+                        onClick={() => {
+                            console.log("Login button clicked!");
+                            setActiveTab("login");
+                            setShowModal(true)
+                        }}>
+                        Login
+                    </button>
+                    <button
+                        className="land-btn"
+                        onClick={() => {
+                            console.log("Sign Up button clicked!");
+                            setActiveTab("signup");
+                            setShowModal(true);
+                        }}>
+                        Sign Up
+                    </button>
                 </div>
             </div>
         </div>
