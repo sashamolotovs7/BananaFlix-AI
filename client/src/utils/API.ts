f65d41d1ed849a07d31153d706a39ebf1eb25a95
reisleo99 <leonardo.ao.reis@gmail.com>
2024-11-22 18:18:55 -0500

    Adult filter on query

--- client/src/utils/API.ts
+++ client/src/utils/API.ts
@@ -1,5 +1,5 @@
 export const searchMovies = (apiKey: string | undefined, query: string) => {
-    return fetch(`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${query}`);
+    return fetch(`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${query}&include_adult=false`);
   };
 
 
